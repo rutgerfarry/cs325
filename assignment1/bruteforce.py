3b4f9a58a94ae6cd744c99396d1bf3d792e9595f
Rutger Farry <rutgerfarry@gmail.com>
2017-02-02 18:59:28 -0800

    Renamed methods and variables in bruteforce.py

--- assignment1/bruteforce.py
+++ assignment1/bruteforce.py
@@ -1,5 +1,5 @@
 """Determines the closest set of points from a file passed as a command
-line argument"""
+line argument using a brute force algorithm"""
 
 from sys import argv
 from shared import point_list_from_file, Point
@@ -7,14 +7,14 @@ from shared import point_list_from_file, Point
 # Takes a list of points, returns unsorted list of tuples containing
 # two points and the distance between them for every pair of points/
 # Example: [(4, 1), (5, 1)] => [(1.0, (4, 1), (5, 1))]
-def bruteforce(point_list):
+def closest_pair_brute(points):
     distances = []
-    for point1 in point_list:
-        for point2 in point_list:
+    for point1 in points:
+        for point2 in points:
             if point1 != point2:
                 distance_tuple = (Point.distance(point1, point2), point1, point2)
                 distances.append(distance_tuple)
     return distances
 
 POINT_LIST = point_list_from_file(argv[1])
-print bruteforce(POINT_LIST)
+print closest_pair_brute(POINT_LIST)
