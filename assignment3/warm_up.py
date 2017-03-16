4de19893090d4eeb02216ff46df14da2767f07f0
Rutger Farry <rutgerfarry@gmail.com>
2017-03-16 11:35:37 -0700

    Made `warm_up` safer to use if imported as module

--- assignment3/warm_up.py
+++ assignment3/warm_up.py
@@ -1,26 +1,30 @@
-from pulp import LpVariable, LpProblem, LpMaximize, LpMinimize, LpStatus, value
+from pulp import LpVariable, LpProblem, LpMinimize, LpStatus, value
 
-points = [(1, 3),
-          (2, 5),
-          (3, 7),
-          (5, 11),
-          (7, 14),
-          (8, 15),
-          (10, 19)]
+def warm_up():
+    points = [(1, 3),
+              (2, 5),
+              (3, 7),
+              (5, 11),
+              (7, 14),
+              (8, 15),
+              (10, 19)]
 
-prob = LpProblem("warmUp", LpMinimize)
+    prob = LpProblem("warmUp", LpMinimize)
 
-m = LpVariable("m")
-a = LpVariable("a")
-b = LpVariable("b")
-prob += m
+    m = LpVariable("m")
+    a = LpVariable("a")
+    b = LpVariable("b")
+    prob += m
 
-# Make each point a constraint
-for point in points:
-    prob += a * point[0] + b - point[1] <= m
-    prob += -a * point[0] - b + point[1] <= m
-    
-status = prob.solve()
-print status, LpStatus[status]
-print "a: {0} b: {1}".format(value(a), value(b))
+    # Make each point a constraint
+    for point in points:
+        prob += a * point[0] + b - point[1] <= m
+        prob += -a * point[0] - b + point[1] <= m
 
+    status = prob.solve()
+    print status, LpStatus[status]
+    print "a: {0} b: {1}".format(value(a), value(b))
+
+# Prevent running if imported as a module
+if __name__ == "__main__":
+    warm_up()
